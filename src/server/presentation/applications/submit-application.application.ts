d825c21f48b70b25307ba772da05b950d4ad38f5
bunyawatapp37204 <stonksfarm37204@gmail.com>
2025-04-13 21:53:05 +0700

    feat: submit application usecase

--- src/server/presentation/applications/submit-application.application.ts
+++ src/server/presentation/applications/submit-application.application.ts
@@ -1,13 +1,11 @@
 import { z } from "zod";
 
-import { PrismaClient } from "@prisma/client";
 import { SubmitApplicationUseCase } from "../../application/usecases/submit-dorm.usecase";
 import { PrismaActivityRepository } from "../../infrastructure/database/repositories/activity.repository";
 import { PrismaApplicationRepository } from "../../infrastructure/database/repositories/application.repository";
-import { publicProcedure, router } from "../base";
+import { prisma, publicProcedure, router } from "../base";
 import { errorMiddleware } from "../middlewares/error.middleware";
 
-const prisma = new PrismaClient();
 const activityRepo = new PrismaActivityRepository(prisma);
 const applicationRepo = new PrismaApplicationRepository(prisma);
 
--- src/server/presentation/base.ts
+++ src/server/presentation/base.ts
@@ -1,4 +1,5 @@
 // src/trpc/base.ts
+import { PrismaClient } from "@prisma/client";
 import { initTRPC } from "@trpc/server";
 
 const t = initTRPC.create();
@@ -6,3 +7,4 @@ const t = initTRPC.create();
 export const router = t.router;
 export const middleware = t.middleware;
 export const publicProcedure = t.procedure;
+export const prisma = new PrismaClient();
