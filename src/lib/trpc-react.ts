2b69b1a7fe2902ae2e39ec0b65a839767a953cb6
bunyawatapp37204 <stonksfarm37204@gmail.com>
2025-04-20 04:43:17 +0700

    fix: api url

--- src/lib/trpc-react.ts
+++ src/lib/trpc-react.ts
@@ -3,7 +3,8 @@ import { createTRPCReact } from "@trpc/react-query";
 import type { AppRouter } from "../server/presentation/index"; // Import the AppRouter type
 
 export const trpc = createTRPCReact<AppRouter>(); // Create React tRPC instance
-const url = process.env.NODE_ENV === "production" ? "https://cunextdorm.vercel.app/api/trpc" : "http://localhost:4321/api/trpc";
+const url =
+	process.env.NODE_ENV === "production" ? "https://cunextdorm.vercel.app/api/trpc" : "http://localhost:4321/api/trpc";
 
 // ✅ Export the tRPC client
 export const trpcClient = trpc.createClient({
