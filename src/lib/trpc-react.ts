88013ddc8136b26b0656d66da54a87ae2a3059b7
bunyawatapp37204 <stonksfarm37204@gmail.com>
2025-04-20 04:41:25 +0700

    fix: api url

--- src/lib/trpc-react.ts
+++ src/lib/trpc-react.ts
@@ -3,12 +3,13 @@ import { createTRPCReact } from "@trpc/react-query";
 import type { AppRouter } from "../server/presentation/index"; // Import the AppRouter type
 
 export const trpc = createTRPCReact<AppRouter>(); // Create React tRPC instance
+const url = process.env.NODE_ENV === "production" ? "https://cunextdorm.vercel.app/api/trpc" : "http://localhost:4321/api/trpc";
 
 // ✅ Export the tRPC client
 export const trpcClient = trpc.createClient({
 	links: [
 		httpBatchLink({
-			url: "http://localhost:4321/api/trpc", // Ensure this is correct
+			url: url, // Ensure this is correct
 		}),
 	],
 });
--- src/lib/trpc.ts
+++ src/lib/trpc.ts
@@ -2,11 +2,11 @@ import { createTRPCClient } from "@trpc/client";
 import type { AppRouter } from "../server/presentation/index";
 
 import { httpBatchLink } from "@trpc/client";
-
+const url = process.env.NODE_ENV === "production" ? "https://cunextdorm.vercel.app/api/trpc" : "http://localhost:4321/api/trpc";
 export const trpc = createTRPCClient<AppRouter>({
 	links: [
 		httpBatchLink({
-			url: "http://localhost:4321/api/trpc",
+			url: url,
 		}),
 	],
 });
